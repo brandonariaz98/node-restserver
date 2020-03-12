520120b80ca8158d2c1a565b33508fd40f08a9cd
Brandon Arias <2017030706@upsin.edu.mx>
2020-03-12 00:11:50 -0700

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,20 @@
 // Puerto
 //=============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+// Entorno
+//=============================
+process.eventNames.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+// Base de Datos
+//=============================
+let urlDB;
+
+if ( process.env.NODE_ENV === 'dev' ){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://urek:01vyXQ3AjUNnmHct@cluster0-iwkur.mongodb.net/test';
+}
+process.env.URLDB = urlDB;
